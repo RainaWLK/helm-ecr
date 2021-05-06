270b371f858db65a4ef5eda1cfbfc09b51060c5e
RainaWLK <lukeshih72@gmail.com>
2021-05-06 18:48:47 +0800

    ci: release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VERSION=0.1.3
+
+rm helm-ecr_${VERSION}_darwin_amd64.tar.gz
+
+rm -Rf package
+mkdir package
+
+cp plugin.yaml package/
+cp -R bin package/
+cp LICENSE package/
+
+tar zcf helm-ecr_${VERSION}_darwin_amd64.tar.gz -C package .
